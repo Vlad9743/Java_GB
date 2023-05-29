b27955a84c37df96c66ddccb570566b10b7a8f66
Vlad <blackboxorange@gmail.com>
2023-05-30 00:00:57 +0400

    OOP lesson1

---
+++ src/main/java/org/example/OOP1_Vlad_Skrebkov/FamilyTree.java
@@ -0,0 +1,39 @@
+package org.example.OOP1_Vlad_Skrebkov;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+    private List<Human> humanList;
+
+    public FamilyTree(){
+        humanList = new ArrayList<>();
+    }
+
+    public void addHuman(Human human){
+        humanList.add(human);
+    }
+
+    public List<Human> findBrothers(Human humanForSearch){
+        List<Human> brothersList = new ArrayList<>();
+
+        for(Human item : humanList){
+            if(item.getFather() != null) {
+                if (item.getFather().equals(humanForSearch.getFather())
+                        && item.getGender() == Gender.MALE
+                        && !item.getName().equals(humanForSearch.getName())) {
+                    brothersList.add(item);
+                }
+            }
+        }
+        return brothersList;
+    }
+
+    public void printHumanList(){
+        for(Human item : humanList){
+            System.out.println(item.toString());
+        }
+    }
+
+
+}
---
+++ src/main/java/org/example/OOP1_Vlad_Skrebkov/Gender.java
@@ -0,0 +1,5 @@
+package org.example.OOP1_Vlad_Skrebkov;
+
+public enum Gender {
+ MALE,  FEMALE, UNKNOWN
+}
